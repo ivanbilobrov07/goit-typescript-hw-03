b733ab1d130eb75f2ecaff57626cb1d3cc8ab47e
Ivan Bilobrov <ivanbilobrov07@gmail.com>
2023-10-02 20:16:10 +0300

    first commit

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  protected key: Key;
+  tenants: Person[];
+
+  constructor(key: Key) {
+    this.tenants = [];
+    this.key = key;
+  }
+
+  comeIn(p: Person) {
+    if (this.door) {
+      this.tenants.push(p);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key) {
+    if (key === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +61,3 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
